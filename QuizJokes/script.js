6e3d1872421188e2ca8309a19c0e292a4ff5f444
Sahrul Romadi <sahrulromadi12@gmail.com>
2025-01-15 22:35:46 +0700

    fix[QuizJokes]: fix bug waktu, buat fitur start game, dan update tampilan

--- QuizJokes/script.js
+++ QuizJokes/script.js
@@ -1,5 +1,3 @@
-// bug: ketika jawaban benar, countdown berjalan
-
 const questions = [
   {
     question: "Seniman yang seneng karyanya di injak-injak adalah...",
@@ -20,84 +18,29 @@ const questions = [
   },
 ];
 
-const countdown = () => {
-  const countdownElement = document.getElementById("countdown");
-
-  // menangani countdownInterval masih ada
-  if (countdownInterval) {
-    clearInterval(countdownInterval);
-  }
-
-  countdownInterval = setInterval(() => {
-    countdownElement.innerText = `${time}`;
-    time--;
-
-    if (time < 0) {
-      // soal terakhir
-      if (number === questions.length - 1) {
-        number++;
-        showModal("Jawaban benar");
-        nextQuestion();
-      } else {
-        clearInterval(countdownInterval);
-        showModal("Waktu habis");
-      }
-    }
-  }, 1000); // 1 detik
-};
-
-const clearInput = () => {
-  const formElement = document.querySelector("form");
-  formElement.reset();
-};
-
-const showModal = (text) => {
-  const modal = document.getElementById("modal");
-  const ok = document.getElementById("ok");
-  const statusJawaban = modal.querySelector("p");
-
-  modal.classList.remove("hidden");
-  clearInterval(countdownInterval);
-
-  statusJawaban.innerText = text;
-
-  ok.addEventListener("click", () => {
-    modal.classList.add("hidden");
-    clearInput();
-
-    // menangani countdown tidak ke reset
-    time = 10;
-    countdown();
-  });
-};
-
-const submitLogic = () => {
-  const submitButton = document.querySelector("button[type='submit']");
-  const answerUser = document.getElementById("answer");
-
-  submitButton.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    if (number < questions.length) {
-      if (questions[number].answer === answerUser.value) {
-        showModal("Jawaban benar");
-        number++;
-        clearInterval(countdownInterval);
-        nextQuestion();
-      } else {
-        showModal("Jawaban salah");
-      }
-    }
-  });
-};
-
-// MAIN
 const question = document.getElementById("question");
 const bar = document.getElementById("bar");
 const content = document.getElementById("content");
+const startElement = document.getElementById("start");
+
+let start = false;
 let number = 0;
 let time = 10;
 let countdownInterval;
+
+const onStart = () => {
+  const startButton = document.getElementById("startButton");
+
+  startButton.addEventListener("click", () => {
+    start = true;
+
+    startElement.classList.add("hidden");
+    content.classList.remove("hidden");
+    bar.classList.remove("hidden");
+
+    nextQuestion();
+  });
+};
 
 const nextQuestion = () => {
   if (number >= questions.length) {
@@ -115,4 +58,81 @@ const nextQuestion = () => {
   }
 };
 
-nextQuestion();
+const countdown = () => {
+  const countdownElement = document.getElementById("countdown");
+
+  // menangani countdownInterval masih ada
+  if (countdownInterval) {
+    clearInterval(countdownInterval);
+  }
+
+  if (start) {
+    countdownInterval = setInterval(() => {
+      countdownElement.innerText = `${time}`;
+      time--;
+
+      if (time < 0) {
+        // soal terakhir
+        if (number === questions.length - 1) {
+          showModal("Jawaban benar");
+          number++;
+          nextQuestion();
+        } else {
+          clearInterval(countdownInterval);
+          showModal("Waktu habis");
+        }
+      }
+    }, 1000); // 1 detik
+  }
+};
+
+const showModal = (text) => {
+  const modal = document.getElementById("modal");
+  const ok = document.getElementById("ok");
+  const statusJawaban = modal.querySelector("p");
+
+  modal.classList.remove("hidden");
+  statusJawaban.innerText = text;
+
+  ok.addEventListener("click", () => {
+    modal.classList.add("hidden");
+    clearInput();
+
+    // menangani countdown tidak ke reset
+    time = 10;
+
+    // setelah modal ditutup, tampilkan next question
+    nextQuestion();
+  });
+};
+
+const submitLogic = () => {
+  const submitButton = document.querySelector("button[type='submit']");
+
+  // hapus event listener terlebih dahulu biar ga duplikat
+  submitButton.removeEventListener("click", handleSubmit);
+  submitButton.addEventListener("click", handleSubmit);
+};
+
+const handleSubmit = (e) => {
+  const answerUser = document.getElementById("answer");
+
+  e.preventDefault();
+
+  if (questions[number].answer === answerUser.value) {
+    showModal("Jawaban benar");
+    // hentikan timer
+    clearInterval(countdownInterval);
+    // tambah number
+    number++;
+  } else {
+    showModal("Jawaban salah");
+  }
+};
+
+const clearInput = () => {
+  const formElement = document.querySelector("form");
+  formElement.reset();
+};
+
+onStart();
